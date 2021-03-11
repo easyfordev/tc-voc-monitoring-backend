fc3cb0f6c713933a36e58c4815bbba1b2375e0e0
easyfordev <easyfordev8@gmail.com>
2021-03-11 21:01:52 +0900

    Upload some part of codes

---
+++ src/test/java/com/skt/infra/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skt.infra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
